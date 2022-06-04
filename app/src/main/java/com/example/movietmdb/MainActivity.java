5dd78b07e8ee3eaa7b0ba799c88100da2ed2df36
Ashif119 <106506030+Ashif119@users.noreply.github.com>
2022-06-04 17:27:01 +0530

    oo

--- app/src/main/java/com/example/movietmdb/MainActivity.java
+++ app/src/main/java/com/example/movietmdb/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.movietmdb;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.ViewDialog;
 import com.example.movietmdb.adapter.MoviesAdapter;
 import com.example.movietmdb.model.Movie;
 import com.example.movietmdb.model.MovieResponse;
@@ -28,38 +45,41 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String BASE_URL = "https://api.themoviedb.org";
-    public static final String lang ="en-US";
+    public static final String lang = "en-US";
 
 
     public static final String API_KEY = "55957fcf3ba81b137f8fc01ac5a31fb5";
     private static Retrofit retrofit = null;
     private RecyclerView recyclerView = null;
 
+    private LinearLayout linyar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linyar=findViewById(R.id.linyar);
         ImageSlider imageSlider = findViewById(R.id.slider);
-        List<SlideModel> slideModels=new ArrayList<>();
-        slideModels.add(new SlideModel(R.drawable.icon, ScaleTypes.CENTER_CROP));
-        slideModels.add(new SlideModel(R.drawable.doctor, ScaleTypes.CENTER_CROP));
+        List<SlideModel> slideModels = new ArrayList<>();
+//        slideModels.add(new SlideModel(R.drawable.icon, ScaleTypes.CENTER_CROP));
         slideModels.add(new SlideModel(R.drawable.ffbeast, ScaleTypes.CENTER_CROP));
         slideModels.add(new SlideModel(R.drawable.morbious, ScaleTypes.CENTER_CROP));
         slideModels.add(new SlideModel(R.drawable.thelostcity, ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.doctor, ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
 
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         connectAndGetApiData();
     }
+
     // This method create an instance of Retrofit
 // set the base url
-    public void connectAndGetApiData(){
+    public void connectAndGetApiData() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
@@ -68,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         }
         MovieApiService movieApiService = retrofit.create(MovieApiService.class);
         Call<MovieResponse> call = movieApiService.listMovie(
-                API_KEY,lang,"1"
+                API_KEY, lang, "1"
         );
         call.enqueue(new Callback<MovieResponse>() {
             @Override
@@ -77,10 +97,141 @@ public class MainActivity extends AppCompatActivity {
                 recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
                 Log.d(TAG, "Number of movies received: " + movies.size());
             }
+
             @Override
             public void onFailure(Call<MovieResponse> call, Throwable throwable) {
                 Log.e(TAG, throwable.toString());
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+//        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+//        builder.setMessage("Are You Sure Want to Exit")
+//                .setCancelable(false)
+//                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                MainActivity.this.finish();
+//                            }
+//                        })
+//                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                                    @Override
+//                                    public void onClick(DialogInterface dialog, int which) {
+//                                        dialog.cancel();
+//                                    }
+//                                });
+//        AlertDialog alertDialog=builder.create();
+//        alertDialog.show();
+
+//        Intent intent = new Intent(MainActivity.this,ViewDialog.class);
+//        startActivity(intent);
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.costum_dialog_box);
+//        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        TextView text = (TextView) dialog.findViewById(R.id.t1);
+
+
+        Button dialogBtn_cancel = (Button) dialog.findViewById(R.id.btnNo);
+        dialogBtn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                    Toast.makeText(getApplicationContext(),"Cancel" ,Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        Button dialogBtn_okay = (Button) dialog.findViewById(R.id.btnYes);
+        dialogBtn_okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                    Toast.makeText(getApplicationContext(),"Okay" ,Toast.LENGTH_SHORT).show();
+                MainActivity.this.finish();
+                dialog.cancel();
+            }
+        });
+        dialog.show();
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!isNetworkAvailable() == true)
+//        {
+//            new AlertDialog.Builder(this)
+//                    .setIcon(android.R.drawable.ic_dialog_alert)
+//                    .setTitle("Internet Connection Alert")
+//                    .setMessage("Please Check Your Internet Connection")
+//                    .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialogInterface, int i) {
+//                            finish();
+//                        }
+//                    }).show();
+
+
+//        }
+        {
+            linyar.setVisibility(View.GONE);
+            final Dialog dialog = new Dialog(this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.costum_dialog_internet);
+//        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+            TextView text = (TextView) dialog.findViewById(R.id.t1);
+
+
+            Button dialogBtn_cancel = (Button) dialog.findViewById(R.id.btnNo);
+            dialogBtn_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Toast.makeText(getApplicationContext(),"Cancel" ,Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    MainActivity.this.finish();
+                }
+            });
+            dialog.show();
+        } else if (isNetworkAvailable() == true) {
+            Toast.makeText(MainActivity.this,
+                    "Welcome", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public boolean isNetworkAvailable() {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+
+
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+                if (capabilities != null) {
+                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+
+                        return true;
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+
+                        return true;
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+
+
+    }
+
 }
